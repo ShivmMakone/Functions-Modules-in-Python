cb68c496cd7a8ced47c6b1b3802873ff7b1f6e75
ShivmMakone <shivammakone@gmail.com>
2025-02-28 05:54:11 +0530

    Add files via upload
    To calculate the square root, natural logarithm, and sine of a given number

---
+++ Task2.py
@@ -0,0 +1,11 @@
+import math
+
+number = float(input("Enter a number: "))
+
+sqrt = math.sqrt(number)
+log= math.log(number)
+sine= math.sin(number)
+
+print(f"Square root is: {sqrt}")
+print(f"Natural logarithm is: {log}")
+print(f"Sine of is: {sine}")
